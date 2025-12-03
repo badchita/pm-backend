2ac29d87f8a6cfa77ccf240a86afd6a13087c9f1
badchita <47920377+badchita@users.noreply.github.com>
2025-12-03 16:50:51 +0800

    getProjectById API

--- pm-backend/Services/Commands/Contracts/IProjectService.cs
+++ pm-backend/Services/Commands/Contracts/IProjectService.cs
@@ -6,5 +6,6 @@ namespace pm_backend.Services.Commands.Contracts
     public interface IProjectService
     {
         Task<Project> CreateProject(CreateProjectRequest request, string userEmail);
+        Task<Project?> GetProjectByIdAsync(int id, string userEmail);
     }
 }
