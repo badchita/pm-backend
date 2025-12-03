2240fafd0500552d259f0d02baf04fcf35198ca8
badchita <47920377+badchita@users.noreply.github.com>
2025-12-03 16:58:25 +0800

    update project dto

---
+++ pm-backend/DTOs/UpdateProjectRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace pm_backend.DTOs
+{
+    public class UpdateProjectRequest
+    {
+        [Required]
+        [MaxLength(150)]
+        public string ProjectName { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        [MaxLength(1)]
+        public string? IsPublished { get; set; }
+
+        [MaxLength(1)]
+        public string? IsDeleted { get; set; }
+    }
+}
