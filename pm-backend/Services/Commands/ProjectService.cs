5dcf556e385c8cf504997c5b0d0bd178abf6845b
badchita <47920377+badchita@users.noreply.github.com>
2025-12-04 09:18:33 +0800

    update project service

--- pm-backend/Services/Commands/ProjectService.cs
+++ pm-backend/Services/Commands/ProjectService.cs
@@ -45,6 +45,25 @@ namespace pm_backend.Services.Commands
             return project;
         }
 
+        public async Task<Project?> UpdateProjectAsync(int id, UpdateProjectRequest request, string userEmail)
+        {
+            var project = await _context.Projects.FirstOrDefaultAsync(x => x.Id == id && x.IsDeleted == "N");
+
+            if (project == null)
+                return null;
+
+            project.ProjectName = request.ProjectName;
+            project.Description = request.Description;
+            project.IsPublished = request.IsPublished ?? project.IsPublished;
+            project.IsDeleted = request.IsDeleted ?? project.IsDeleted;
+            project.DueDate = request.DueDate;
+
+            await _context.SaveChangesAsync();
+
+            return project;
+        }
+
+
 
         private async Task<string> GenerateProjectNumber()
         {
