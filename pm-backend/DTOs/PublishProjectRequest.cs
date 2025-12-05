6382473383deb2bb2816eaa8c29e3bcc5c9ac9af
badchita <47920377+badchita@users.noreply.github.com>
2025-12-05 19:25:23 +0800

    publish project dto

---
+++ pm-backend/DTOs/PublishProjectRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace pm_backend.DTOs
+{
+    public class PublishProjectRequest
+    {
+        public string? ProjectName { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
