7d1b6d107eac2e8310580c9fe3ebcfca8594884e
badchita <47920377+badchita@users.noreply.github.com>
2025-12-17 18:59:25 +0800

    update task service

--- pm-backend/Services/Commands/Contracts/IProjectTaskService.cs
+++ pm-backend/Services/Commands/Contracts/IProjectTaskService.cs
@@ -7,5 +7,6 @@ namespace pm_backend.Services.Commands.Contracts
     {
         Task<ProjectTask> CreateTaskAsync(CreateProjectTaskRequest request, string userEmail);
         Task<ProjectTask?> GetTaskByIdAsync(int id, int projectId, string userEmail);
+        Task<ProjectTask?> UpdateTaskAsync(int id, int projectId, CreateProjectTaskRequest request, string userEmail);
     }
 }
