4fbd7a85bc628b8f7cb08543de7d1c2e20143bd2
badchita <47920377+badchita@users.noreply.github.com>
2025-12-20 15:43:40 +0800

    task comment service

---
+++ pm-backend/Services/Queries/Contracts/ITaskCommentQueryService.cs
@@ -0,0 +1,9 @@
+﻿using pm_backend.Models;
+
+namespace pm_backend.Services.Queries.Contracts
+{
+    public interface ITaskCommentQueryService
+    {
+        Task<IReadOnlyList<TaskComment>> GetAllTaskCommentsAsync(int taskId);
+    }
+}
