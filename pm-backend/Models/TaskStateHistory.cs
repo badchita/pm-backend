bfa40e5f4f26142cad1774646b8bb0a4dbbea458
badchita <47920377+badchita@users.noreply.github.com>
2025-12-21 17:58:47 +0800

    task state histories migration

---
+++ pm-backend/Models/TaskStateHistory.cs
@@ -0,0 +1,28 @@
+﻿using pm_backend.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class TaskStateHistory
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public int TaskId { get; set; }
+
+    [ForeignKey("TaskId")]
+    public ProjectTask Task { get; set; }
+
+    [Required]
+    public TaskState PreviousState { get; set; }
+
+    [Required]
+    public TaskState NewState { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string ChangedBy { get; set; }
+
+    [Required]
+    public DateTime ChangedAt { get; set; } = DateTime.UtcNow;
+}
