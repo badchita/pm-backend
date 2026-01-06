3997c61a3acab3cac43f9bab3950f0a091d52c6f
badchita <47920377+badchita@users.noreply.github.com>
2026-01-06 15:42:45 +0800

    company query service

---
+++ pm-backend/Services/Queries/CompanyQueryService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using pm_backend.Data;
+using pm_backend.Models;
+using pm_backend.Services.Queries.Contracts;
+
+namespace pm_backend.Services.Queries
+{
+    public class CompanyQueryService : ICompanyQueryService
+    {
+        private readonly PmDbContext _context;
+
+        public CompanyQueryService(PmDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<Company>> GetAllCompaniesAsync(string? search)
+        {
+            IQueryable<Company> query = _context.Companies;
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                query = query.Where(u =>
+                    u.Name.Contains(search)
+                );
+            }
+
+            return await query.ToListAsync();
+        }
+    }
+}
---
+++ pm-backend/Services/Queries/Contracts/ICompanyQueryService.cs
@@ -0,0 +1,9 @@
+﻿using pm_backend.Models;
+
+namespace pm_backend.Services.Queries.Contracts
+{
+    public interface ICompanyQueryService
+    {
+        Task<IReadOnlyList<Company>> GetAllCompaniesAsync(string? search);
+    }
+}
