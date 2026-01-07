3c02ed17123ad3c0ae43e663c700725a88d80796
badchita <47920377+badchita@users.noreply.github.com>
2026-01-07 10:49:30 +0800

    update user service

--- pm-backend/Services/Commands/Contracts/IUserService.cs
+++ pm-backend/Services/Commands/Contracts/IUserService.cs
@@ -1,7 +1,11 @@
-﻿namespace pm_backend.Services.Commands.Contracts
+﻿using pm_backend.DTOs;
+using pm_backend.Models;
+
+namespace pm_backend.Services.Commands.Contracts
 {
     public interface IUserService
     {
         Task UpdateIsDeletedAsync(int id, string isDeleted);
+        Task<User?> UpdateUserAsync(int id, UserDto request);
     }
 }
